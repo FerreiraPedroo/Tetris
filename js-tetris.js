e6fb41b3dda319d57b30a6e55f1d1014849a6049
Pedro Henrique de Assis Ferreira <73146680+Simplerino@users.noreply.github.com>
2021-06-13 11:45:44 -0300

    Ajustes iniciais
    Ajustes iniciais, github ainda em treinamento.

--- js-tetris.js
+++ js-tetris.js
@@ -34,20 +34,34 @@ let blocoPosX = 0;
 let blocoPosY = 0;
 
 
-
-
 criarNovoBloco()
 function criarNovoBloco() {
-    let random = parseInt(Math.random() * 1);
     matrizJogo[blocoPosX][blocoPosY] = 2
     matrizJogo[blocoPosX+1][blocoPosY] = 2
     matrizJogo[blocoPosX+2][blocoPosY] = 2
     matrizJogo[blocoPosX+3][blocoPosY] = 2
 }
+function movendoBloco (){
 
+}
+
+
+
+function limpandoMatrizJogo(){
+    for (x = 0; x < matrizJogo.length; x++) {
+        for (y = 0; y < matrizJogo[0].length; y++) {
+            if(matrizJogo[x][y] == 2){
+                matrizJogo[x][y] == 0;
+            }
+            // console.log(`linha-${x}-coluna-${y}}`)
+        }
+    }
+    atualizandoMatrizJogo()
+}
 atualizandoMatrizJogo()
-function atualizandoMatrizJogo() {
 
+
+function atualizandoMatrizJogo() {
     for (x = 0; x < matrizJogo.length; x++) {
         for (y = 0; y < matrizJogo[0].length; y++) {
             document.getElementById(`linha-${x}-coluna-${y}`).innerText = matrizJogo[x][y];
@@ -55,6 +69,8 @@ function atualizandoMatrizJogo() {
         }
     }
 }
+
+
 
 
 
@@ -78,21 +94,33 @@ movimentação do bloco.
 function movimentandoBloco(_key) {
     switch (_key) {
         case keyConfig[0]:
-            console.log("CIMA")
-            break
+            limpandoMatrizJogo()
+            console.log("CIMA");
+            break;
+
+
         case keyConfig[1]:
+            limpandoMatrizJogo()
             if (blocoPosX >= 0 && blocoPosX < matrizJogo[0].length) {
                 blocoPosX += 1;
             }
             break
+
+
         case keyConfig[2]:
-            console.log("BAIXO")
+            limpandoMatrizJogo()
+            console.log("BAIXO");
             break
+
+
         case keyConfig[3]:
+            limpandoMatrizJogo()
             if (blocoPosX > 0 && blocoPosX <= matrizJogo[0].length) {
                 blocoPosX -= 1;
             }
             break
+
+
         default:
             console.log("KEY - N/A")
             break
