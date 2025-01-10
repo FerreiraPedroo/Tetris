72b44ee83bb240cd5c23583fd8b07a9d767ecba6
Pedro Henrique de Assis Ferreira <73146680+FerreiraPedroo@users.noreply.github.com>
2025-01-10 12:03:37 -0300

    Update js-tetris.js

--- js-tetris.js
+++ js-tetris.js
@@ -1,131 +1,121 @@
 class Tetris {
-    constructor() {
-    }
+  constructor() {}
 }
 
 let matrizJogo = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-]
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
 
-const bloco = [
-    [2, 2, 2, 2]
-]
+const bloco = [[2, 2, 2, 2]];
 
 let blocoPosX = 0;
 let blocoPosY = 0;
+const novoBlocoPosicao = [0, 5];
 
-
-criarNovoBloco()
+criarNovoBloco();
 function criarNovoBloco() {
-    matrizJogo[blocoPosX][blocoPosY] = 2
-    matrizJogo[blocoPosX+1][blocoPosY] = 2
-    matrizJogo[blocoPosX+2][blocoPosY] = 2
-    matrizJogo[blocoPosX+3][blocoPosY] = 2
+  blocoPosX = novoBlocoPosicao[0];
+  blocoPosY = novoBlocoPosicao[1];
+
+  bloco.forEach((linha, linhaIndex) => {
+    linha.forEach((coluna, colunaIndex) => {
+      matrizJogo[colunaIndex + novoBlocoPosicao[0]][novoBlocoPosicao[1]] =
+        coluna;
+    });
+  });
 }
-function movendoBloco (){
 
-}
-
-
-
-function limpandoMatrizJogo(){
-    for (x = 0; x < matrizJogo.length; x++) {
-        for (y = 0; y < matrizJogo[0].length; y++) {
-            if(matrizJogo[x][y] == 2){
-                matrizJogo[x][y] == 0;
-            }
-            // console.log(`linha-${x}-coluna-${y}}`)
-        }
+function limpandoMatrizJogo() {
+  for (x = 0; x < matrizJogo.length; x++) {
+    for (y = 0; y < matrizJogo[0].length; y++) {
+      if (matrizJogo[x][y] == 2) {
+        matrizJogo[x][y] == 0;
+      }
     }
-    atualizandoMatrizJogo()
+  }
+  atualizandoMatrizJogo();
 }
-atualizandoMatrizJogo()
 
+function atualizandoPosicaoBlocoAtivo() {
+  
+    bloco.forEach((linha, linhaIndex) => {
+      linha.forEach((coluna, colunaIndex) => {
+        matrizJogo[blocoPosY][novoBlocoPosicao[1]] =
+          coluna;
+      });
+    });
+}
 
 function atualizandoMatrizJogo() {
-    for (x = 0; x < matrizJogo.length; x++) {
-        for (y = 0; y < matrizJogo[0].length; y++) {
-            document.getElementById(`linha-${x}-coluna-${y}`).innerText = matrizJogo[x][y];
-            // console.log(`linha-${x}-coluna-${y}}`)
-        }
+  for (x = 0; x < matrizJogo.length; x++) {
+    for (y = 0; y < matrizJogo[0].length; y++) {
+      document.getElementById(`linha-${x}-coluna-${y}`).innerText =
+        matrizJogo[x][y];
     }
+  }
 }
 
-
-
-
-
+function detectarColisao() {}
 
 /*
 Array de configuração da tecla de movimentação dos blocos.
 */
-let keyConfig = [101, 99, 98, 97]
+let keyConfig = { ArrowDown: 1, ArrowRight: 1, ArrowLeft: -1 };
 
 /*
 detecção da tecla digitada.
 */
-document.querySelector('body').addEventListener('keydown', function (event) {
-    movimentandoBloco(event.keyCode);
-    //console.log(event.keyCode)
+document.querySelector("body").addEventListener("keydown", function (event) {
+  movimentandoBloco(event.key);
 });
 
 /*
 movimentação do bloco.
 */
 function movimentandoBloco(_key) {
-    switch (_key) {
-        case keyConfig[0]:
-            limpandoMatrizJogo()
-            console.log("CIMA");
-            break;
+  switch (_key) {
+    case "ArrowRight":
+      limpandoMatrizJogo();
+      if (blocoPosX >= 0 && blocoPosX < matrizJogo[0].length) {
+        blocoPosX += 1;
+      }
+      break;
 
+    case "ArrowDown":
+      limpandoMatrizJogo();
+      console.log("BAIXO");
+      break;
 
-        case keyConfig[1]:
-            limpandoMatrizJogo()
-            if (blocoPosX >= 0 && blocoPosX < matrizJogo[0].length) {
-                blocoPosX += 1;
-            }
-            break
+    case "ArrowLeft":
+      limpandoMatrizJogo();
+      if (blocoPosX > 0 && blocoPosX <= matrizJogo[0].length) {
+        blocoPosX -= 1;
+      }
+      break;
 
-
-        case keyConfig[2]:
-            limpandoMatrizJogo()
-            console.log("BAIXO");
-            break
-
-
-        case keyConfig[3]:
-            limpandoMatrizJogo()
-            if (blocoPosX > 0 && blocoPosX <= matrizJogo[0].length) {
-                blocoPosX -= 1;
-            }
-            break
-
-
-        default:
-            console.log("KEY - N/A")
-            break
-    }
-    console.log("DIREITA: " + blocoPosX);
-    console.log("ESQUERDA: " + blocoPosY);
+    default:
+      console.log("KEY - N/A");
+      break;
+  }
+  console.log("DIREITA: " + blocoPosX);
+  console.log("ESQUERDA: " + blocoPosY);
 }
-
